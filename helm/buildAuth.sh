0f9cf73542b3a9fd2cd63d320d3b20bd83a27fa3
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2019-01-31 07:41:43 -0500

    Command Update

--- helm/buildAuth.sh
+++ helm/buildAuth.sh
@@ -2,9 +2,9 @@
 helm install --replace --name nodechart ./nodechart --set service.type=NodePort
 # If you need to delete:
 # helm delete nodechart
-k apply -f pig.za261.io/nginxauth-service.yaml
-k apply -f pig.za261.io/nginxauth-ingress.yaml
-k apply -f pig.za261.io/nginxauth-deployment.yaml
+kubectl apply -f pig.za261.io/nginxauth-service.yaml
+kubectl apply -f pig.za261.io/nginxauth-ingress.yaml
+kubectl apply -f pig.za261.io/nginxauth-deployment.yaml
 #
 # Watch services
 # k get service -w
